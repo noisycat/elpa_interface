bf5be350e22a25b1cadc5a2134ca3b416cfa9632
Charles <ctxlena@yahoo.com>
2016-09-23 22:30:00 -0500

    I had to patch the elpa_generated.h file installed to work properly with C++. It didn't have header guards, nor proper externing statements. I'll probably try and fix that for them in the repo. Also, note, precision.mod is required by the modules, but not installed. Certainly someone noticed this...

---
+++ patch.elpa_generated.h
@@ -0,0 +1,21 @@
+1c1,10
+<  #include <complex.h>
+---
+> #ifndef __ELPA_GENERATED_H__ 
+> #define __ELPA_GENERATED_H__ 
+> 
+> #include <complex.h>
+> #ifdef __cplusplus
+> #undef complex
+> #define complex _Complex
+> extern "C" {
+> #endif
+> 
+11d19
+<  #include <complex.h>
+275a284,288
+> #undef complex
+> #ifdef __cplusplus
+> }
+> #endif
+> #endif
